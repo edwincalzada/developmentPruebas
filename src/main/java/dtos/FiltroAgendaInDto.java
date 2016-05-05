b163218ef58ce7452d0e24d727fb08445d02ee66
edwincalzada <cal.edw.3@gmail.com>
2016-05-05 11:51:35 -0500

    cambios .gitignore

--- src/main/java/dtos/FiltroAgendaInDto.java
+++ src/main/java/dtos/FiltroAgendaInDto.java
@@ -2,4 +2,39 @@ package dtos;
 
 public class FiltroAgendaInDto {
 
+	private String titulo;
+	private String evento;
+	private String fechaDeRegistro;
+	private String fechaDeEvento;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(final String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getEvento() {
+		return evento;
+	}
+
+	public void setEvento(final String evento) {
+		this.evento = evento;
+	}
+
+	public String getFechaDeRegistro() {
+		return fechaDeRegistro;
+	}
+
+	public void setFechaDeRegistro(final String fechaDeRegistro) {
+		this.fechaDeRegistro = fechaDeRegistro;
+	}
+
+	public String getFechaDeEvento() {
+		return fechaDeEvento;
+	}
+
+	public void setFechaDeEvento(final String fechaDeEvento) {
+		this.fechaDeEvento = fechaDeEvento;
+	}
+
 }
--- src/main/java/mx/com/developmentPruebas/developmentPruebas/dao/apis/AgendaDao.java
+++ src/main/java/mx/com/developmentPruebas/developmentPruebas/dao/apis/AgendaDao.java
@@ -1,11 +1,11 @@
 package mx.com.developmentPruebas.developmentPruebas.dao.apis;
 
-import dtos.AgendaDaoDto;
+import dtos.AgendaOutDto;
 import dtos.FiltroAgendaInDto;
 
 public interface AgendaDao {
 
-	AgendaDaoDto obtenerAgenda(FiltroAgendaInDto filtroAgendaInDto);
+	AgendaOutDto obtenerAgenda(FiltroAgendaInDto filtroAgendaInDto);
 
 }
 
